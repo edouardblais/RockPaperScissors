45d17420dd9606ccab5d270ee1a8cddaea0d9f21
edouard <edouard.blais@hotmail.com>
2022-04-07 17:34:18 -0400

    fixed bug in the keeping of score

--- javascript.js
+++ javascript.js
@@ -10,49 +10,55 @@ function PlayRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         alert ("It's a tie!");
-        return 0
+        return 0;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         alert ("You win! Rock beats scissors!");
-        return 1
+        return 1;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         alert ("You lose! Paper beats rock!");
-        return -1
+        return 2;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         alert ("You win! Paper beats rock!");
-        return 1
+        return 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         alert ("You Lose! Scissors beat paper!");
-        return -1
+        return 2;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         alert ("You lose! Rock beats scissors!");
-        return -1
+        return 2;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         alert ("You win! Scissors beat paper!");
-        return 1
+        return 1;
     } else {
         alert ("Oups! I think you mispelled your choice!");
-        return 0
+        return 0;
     }
 
 }
 
 function game() {
 
-    YourScore = 0
-    ComputerScore = 0
-    Ties = 0
+    let YourScore = 0;
+    let ComputerScore = 0;
+    let Ties = 0;
     
     for (let i=0; i<5; i++) {
+        
+        let computerSelection = ComputerPlay();
+
+        let playerSelection = (prompt("Choose between rock, paper or scissors!")).toLowerCase();
+
         result = PlayRound(playerSelection, computerSelection);
-        if (result = 1) {
+
+        if (result === 1) {
             YourScore++;
-        } else if (result = -1) {
+        } else if (result === 2) {
             ComputerScore++;
         } else {
             Ties++;
         }
         
-        alert ("Your score is ${YourScore}, the computer's score is ${ComputerScore} and the number of ties is ${Ties}!");
+        alert (`Your score is ${YourScore}, the computer's score is ${ComputerScore} and the number of ties is ${Ties}!`);
     }
 
     if (YourScore > ComputerScore) {
@@ -63,10 +69,6 @@ function game() {
         alert ("It's a tie game!");
     }   
 }
-
-let computerSelection = ComputerPlay();
-
-let playerSelection = (prompt("Choose between rock, paper or scissors!")).toLowerCase();
 
 console.log(game());
 
