606c71d7f20367ed1f97afe04a92aaa7d00e2cd0
edouard <edouard.blais@hotmail.com>
2022-04-09 14:12:51 -0400

    managed to make it stop at 5 points

--- javascript.js
+++ javascript.js
@@ -1,18 +1,3 @@
-const btnrock = document.querySelector('#rock');
-const btnpaper = document.querySelector('#paper');
-const btnscissors = document.querySelector('#scissors');
-
-btnrock.addEventListener('click', (event) => {
-    PlayRound("rock", ComputerPlay());
-});
-
-btnpaper.addEventListener('click', (event) => {
-    PlayRound("paper", ComputerPlay());
-});
-
-btnscissors.addEventListener('click', (event) => {
-    PlayRound("scissors", ComputerPlay());
-});
 
 
 function ComputerPlay() {
@@ -25,32 +10,43 @@ function ComputerPlay() {
 
 function PlayRound(playerSelection, computerSelection) {
 
+    const container = document.querySelector('#container');
+    const result = document.createElement('div');
+    result.classList.add('result');
+
 
     if (playerSelection === computerSelection) {
-        alert ("It's a tie!");
+        result.textContent="It's a tie!";
+        container.appendChild(result);
         return 0;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        alert ("You win! Rock beats scissors!");
+        result.textContent="You win! Rock beats scissors!";
+        container.appendChild(result);
         return 1;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        alert ("You lose! Paper beats rock!");
+        result.textContent="You lose! Paper beats rock!";
+        container.appendChild(result);
         return 2;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        alert ("You win! Paper beats rock!");
+        result.textContent="You win! Paper beats rock!";
+        container.appendChild(result);
         return 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        alert ("You Lose! Scissors beat paper!");
+        result.textContent="You Lose! Scissors beat paper!";
         return 2;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        alert ("You lose! Rock beats scissors!");
+        result.textContent="You lose! Rock beats scissors!";
+        container.appendChild(result);
         return 2;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        alert ("You win! Scissors beat paper!");
+        result.textContent="You win! Scissors beat paper!";
+        container.appendChild(result);
         return 1;
     } else {
-        alert ("Oups! I think you mispelled your choice!");
+        result.textContent="Oups! I think you mispelled your choice!";
+        container.appendChild(result);
         return 0;
-    }
+    };
 
 }
 
@@ -59,17 +55,57 @@ function game() {
     let YourScore = 0;
     let ComputerScore = 0;
     let Ties = 0;
-    
 
+    const btnrock = document.querySelector('#rock');
+    const btnpaper = document.querySelector('#paper');
+    const btnscissors = document.querySelector('#scissors');
 
-    if (YourScore > ComputerScore) {
-        alert ("Congrats! You won the game!");
-    } else if (YourScore < ComputerScore) {
-        alert ("Too bad! You lost the game!");
-    } else {
-        alert ("It's a tie game!");
-    }   
-}
+    btnrock.addEventListener('click', (event) => {
+        let x = PlayRound("rock", ComputerPlay());
+        if (x==1) {
+            YourScore++;
+        } else if (x==2) {
+            ComputerScore++
+        } else if (x==0) {
+            Ties++
+        };
+        
+        if (YourScore == 5) alert ("You win the game!");
+        if (ComputerScore==5) alert ("You lose the game!");
+    });
+
+    btnpaper.addEventListener('click', (event) => {
+        let x = PlayRound("paper", ComputerPlay());
+        if (x==1) {
+            YourScore++;
+        } else if (x==2) {
+            ComputerScore++
+        } else if (x==0) {
+            Ties++
+        };
+        
+        if (YourScore == 5) alert ("You win the game!");
+        if (ComputerScore==5) alert ("You lose the game!");
+    });
+
+    btnscissors.addEventListener('click', (event) => {
+        let x = PlayRound("scissors", ComputerPlay());
+        if (x==1) {
+            YourScore++;
+        } else if (x==2) {
+            ComputerScore++
+        } else if (x==0) {
+            Ties++
+        };
+        
+        if (YourScore == 5) alert ("You win the game!");
+        if (ComputerScore==5) alert ("You lose the game!");
+    });
+        
+};
+
+game();
+
 
 
 
